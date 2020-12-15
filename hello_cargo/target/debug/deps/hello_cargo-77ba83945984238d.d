962db4a460c68b102ef85e3f421a4a47f69a5760
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-15 10:52:46 -0800

    Hello Cargo!

---
+++ hello_cargo/target/debug/deps/hello_cargo-77ba83945984238d.d
@@ -0,0 +1,5 @@
+/home/pranavnt/RustBook/hello_cargo/target/debug/deps/hello_cargo-77ba83945984238d: src/main.rs
+
+/home/pranavnt/RustBook/hello_cargo/target/debug/deps/hello_cargo-77ba83945984238d.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/pranavnt/RustBook/hello_cargo/target/debug/hello_cargo: /home/pranavnt/RustBook/hello_cargo/src/main.rs
